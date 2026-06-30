c06e8bcda95e78adea0d2255b1bf5f7e1d38831f
Emil Putter <emil.putter@gmail.com>
2026-06-30 14:50:35 +0200

    Implement first HTTP request using reqwest

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+
+ async fn main() {
+
+    println!("Starting ENDA MCP server");
+
+    let response = reqwest:: get(
+       "https://api.hederacourt.site/api/v1/client-classes"
+    )
+    .await
+    .expect("Failed to send request");
+
+    println!("Status: {}", response.status());
+    let body = response
+    .json()
+    .await
+    .expect("Failed to read response body");
+
+    println!("Body: {}", body);
 }
+
